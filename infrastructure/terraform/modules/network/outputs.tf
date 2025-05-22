cca221e337588a665a71b98819b1113baced9f55
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-05-22 23:41:39 +0800

    Create outputs.tf

---
+++ infrastructure/terraform/modules/network/outputs.tf
@@ -0,0 +1,14 @@
+```hcl
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+output "bastion_sg_id" {
+  value = aws_security_group.bastion.id
+}
+```
