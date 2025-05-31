1305dde2f0d1cce27eb71416e262e12c56f8b590
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-05-31 20:03:15 +0800

    Add files via upload

---
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+} 
